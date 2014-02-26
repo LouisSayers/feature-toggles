3e733acc07d0c4ab39b895ae8dc4b9ad63e2b672
Max <max.dupenois@forward.co.uk>
2014-02-26 10:50:49 +0000

    Feature toggling now allows specific user features

--- lib/feature-toggles/feature.rb
+++ lib/feature-toggles/feature.rb
@@ -1,10 +1,32 @@
 class FeatureToggles::Feature
 
-  def self.is_enabled?(feature_name)
-    feature_toggle_data[feature_name][:enabled]
+  def self.is_enabled?(feature_name, user_type=nil)
+    if user_type && has_user_specific_permission?(feature_name, user_type)
+      enabled_for_user?(feature_name, user_type)
+    else
+      enabled_generally?(feature_name)
+    end
   end
 
   private
+
+  def self.enabled_generally?(feature_name)
+    !!feature_permissions(feature_name)[:enabled]
+  end
+
+  def self.has_user_specific_permission?(feature_name, user_type)
+    feature_permissions(feature_name).key?(user_type)
+  end
+
+  def self.enabled_for_user?(feature_name, user_type)
+    !!Hash(feature_permissions(feature_name)[user_type])[:enabled]
+  end
+
+  def self.feature_permissions(feature_name)
+    Hash(feature_toggle_data[feature_name])
+  end
+
+
   def self.default_feature_toggles_file_name
     'feature_toggles.yml'
   end
--- spec/feature_toggles/feature_toggles_spec.rb
+++ spec/feature_toggles/feature_toggles_spec.rb
@@ -11,4 +11,26 @@ describe 'FeatureToggles' do
     expect(another_feature_enabled).to eq true
   end
 
+
+  context "when a feature is enabled for only one user type" do
+    it "should check to see if the specific user type has that feature enabled" do
+      no_user_type_enabled = FeatureToggles::Feature.is_enabled?(:admin_only_feature)
+      user_enabled = FeatureToggles::Feature.is_enabled?(:admin_only_feature, :user)
+      admin_enabled = FeatureToggles::Feature.is_enabled?(:admin_only_feature, :admin)
+      expect(no_user_type_enabled).to be_false
+      expect(user_enabled).to be_false
+      expect(admin_enabled).to be_true
+    end
+  end
+
+  context "when a feature is disabled only for one user type" do
+    it "should check to see if only the specific user type has that feature disnabled" do
+      no_user_type_enabled = FeatureToggles::Feature.is_enabled?(:all_but_user_feature)
+      user_enabled = FeatureToggles::Feature.is_enabled?(:all_but_user_feature, :user)
+      admin_enabled = FeatureToggles::Feature.is_enabled?(:all_but_user_feature, :admin)
+      expect(no_user_type_enabled).to be_true
+      expect(user_enabled).to be_false
+      expect(admin_enabled).to be_true
+    end
+  end
 end
